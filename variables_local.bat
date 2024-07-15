2fa9d493c539d5bb7d045643ba32b42e9eb4db5a
Latha <pai.latha1@gmail.com>
2024-07-15 14:48:32 -0500

    adding variables.bat file that has some changes

--- variables_local.bat
+++ variables_local.bat
@@ -1,13 +1,22 @@
 #!/bin/bash
+#_OVERRIDE
+#DB_DATABASE=localdemostore
+
 
 
 set BASE_URL=http://dev.bootcamp.store.supersqa.com
 set BROWSER=chrome
 set RESULTS_DIR=$(pwd)/results
-set DB_PORT_OVERRIDE=3309
-set DB_HOST_OVERRIDE=localhost
-set DB_DATABASE_OVERRIDE=localdemostore
-set DB_TABLE_PREFIX_OVERRIDE=wp_
+set DB_PORT=3309
+set DB_HOST=dev.bootcamp.store.supersqa.com
+set DB_DATABASE=demostore
+set DB_TABLE_PREFIX=wp_
 
 set PYTHONPATH=C:\Users\Latha\QA-Automation\My_Code\demostore_frontend_backend_tests
+
+# credentials (these should not be kept in source controle like GitHub)
+set WOO_KEY=ck_8169f43e770dc493cae77d7828e227835284050c
+set WOO_SECRET=cs_581ae674c7298f39cf75d30523eb235b25bad1db
+set DB_USER=root
+set DB_PASSWORD=yerootmistir
 
