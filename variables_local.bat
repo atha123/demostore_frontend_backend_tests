848d348ae9a6c8d12d677e398d4c495579d4bd5c
Latha <pai.latha1@gmail.com>
2024-07-19 19:52:12 -0500

    committing the variables.bat file with the woo credentials removed. These are added to a seperate file crediantials.bat which whill not be committed to github

--- variables_local.bat
+++ variables_local.bat
@@ -14,9 +14,3 @@ set DB_TABLE_PREFIX=wp_
 
 set PYTHONPATH=C:\Users\Latha\QA-Automation\My_Code\demostore_frontend_backend_tests
 
-# credentials (these should not be kept in source controle like GitHub)
-set WOO_KEY=ck_8169f43e770dc493cae77d7828e227835284050c
-set WOO_SECRET=cs_581ae674c7298f39cf75d30523eb235b25bad1db
-set DB_USER=root
-set DB_PASSWORD=yerootmistir
-
